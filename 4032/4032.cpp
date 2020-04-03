02bbb56c5391b74246518e4c0825016f250c5407
casc1244 <1356602646@qq.com>
2020-04-03 14:36:50 +0800

    4032.cpp

--- 4032/4032.cpp
+++ 4032/4032.cpp
@@ -1,20 +1,36 @@
-﻿// 4032.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
-//
-
+﻿
+#include "opencv2/highgui/highgui.hpp"
+#include "opencv2/imgproc/imgproc.hpp"
 #include <iostream>
+using namespace cv;
+using namespace std;
+
+
 
 int main()
 {
-    std::cout << "Hello World!\n";
+
+	cv::Mat dstMat;
+	cv::Mat srcMat = cv::imread("D:\\360downloads\\7.png", 1);
+
+	const cv::Point2f src_pt[] = {
+		cv::Point2f(200,200),
+		cv::Point2f(250,200),
+		cv::Point2f(200,100),
+	};
+	const cv::Point2f dst_pt[] = {
+		cv::Point2f(300,100),
+		cv::Point2f(300,50),
+		cv::Point2f(200,100),
+	};
+	const cv::Mat affine_matrix = getAffineTransform(src_pt, dst_pt);
+	cv::warpAffine(srcMat, dstMat, affine_matrix,srcMat.size());
+	imshow("原", srcMat);
+	imshow("换", dstMat);
+
+
+	// 等待用户按任意按键退出程序
+	waitKey(0);
+
+	return 0;
 }
-
-// 运行程序: Ctrl + F5 或调试 >“开始执行(不调试)”菜单
-// 调试程序: F5 或调试 >“开始调试”菜单
-
-// 入门使用技巧: 
-//   1. 使用解决方案资源管理器窗口添加/管理文件
-//   2. 使用团队资源管理器窗口连接到源代码管理
-//   3. 使用输出窗口查看生成输出和其他消息
-//   4. 使用错误列表窗口查看错误
-//   5. 转到“项目”>“添加新项”以创建新的代码文件，或转到“项目”>“添加现有项”以将现有代码文件添加到项目
-//   6. 将来，若要再次打开此项目，请转到“文件”>“打开”>“项目”并选择 .sln 文件
